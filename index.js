68ea94c229c2aad1f4a999d7f6f02705312873ce
Itsredpro <thomhmail@gmail.com>
2023-03-16 10:44:24 +0100

    alpha 1 ready!

---
+++ index.js
@@ -0,0 +1,15 @@
+const alliances = require(__dirname + "/stable/alliance.js")
+const towns = require(__dirname + "/stable/town.js")
+const territory = require(__dirname + "/stable/territory.js")
+const players = require(__dirname + "/stable/player.js")
+const market_buy = require(__dirname + "/stable/market-buy.js")
+const sales = require(__dirname + "/stable/market-sales.js")
+
+module.exports = {
+    "alliance":alliances,
+    "town":towns,
+    "territory":territory,
+    "player":players,
+    "market_sales":sales,
+    "market_buy":market_buy
+}
