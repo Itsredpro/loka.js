9959211e5ff600bf6434f80e382b407cfdf89cba
Itsredpro <thomhmail@gmail.com>
2023-03-16 10:38:54 +0100

    market support + player full list!

---
+++ stable/market-buy.js
@@ -0,0 +1,60 @@
+module.exports.get = {}
+
+module.exports.get.byType = async function(Type){
+    var out = {}
+    try {
+        out = await axios.get("http://testapi.lokamc.com/market_buyorders/search/findByType?type=" + Type)
+    } catch(e){
+
+    }
+
+    if (out.data){
+        return {
+            error:false,
+            data:out.data
+        }
+    } else {
+        return {
+            error:true
+        }
+    }
+}
+
+module.exports.get.fullList = async function(){
+    var out = {}
+    try {
+        out = await axios.get("http://testapi.lokamc.com/market_buyorders?size=1000")
+    } catch(e){
+
+    }
+
+    if (out.data){
+        for (var i = 0; i< out.data.page.totalPages.length; i++){
+            var out = []
+            var error = false
+            try {
+                var res = await axios.get("http://testapi.lokamc.com/market_buyorders?size=1000&page=" + i)
+                for (var i2 = 0; i2< res.data._embedded.market_buyorders.length; i2++){
+                    out.push(res.data._embedded.market_buyorders[i2])
+                }
+            } catch(e){
+                error = true
+            }
+
+            if (error){
+                return {
+                    error:true
+                }
+            } else {
+                return {
+                    error:false,
+                    data:out
+                }
+            }
+        }
+    } else {
+        return {
+            error:true
+        }
+    }
+}
--- stable/player.js
+++ stable/player.js
@@ -41,3 +41,42 @@ module.exports.get.byUuid = async function (uuid) {
         }
     }
 }
+
+module.exports.get.fullList = async function(){
+    var out = {}
+    try {
+        out = await axios.get("https://testapi.lokamc.com/players?size=1000")
+    } catch(e){
+
+    }
+
+    if (out.data){
+        for (var i = 0; i< out.data.page.totalPages.length; i++){
+            var out = []
+            var error = false
+            try {
+                var res = await axios.get("https://testapi.lokamc.com/players?size=1000&page=" + i)
+                for (var i2 = 0; i2< res.data._embedded.players.length; i2++){
+                    out.push(res.data._embedded.players[i2])
+                }
+            } catch(e){
+                error = true
+            }
+
+            if (error){
+                return {
+                    error:true
+                }
+            } else {
+                return {
+                    error:false,
+                    data:out
+                }
+            }
+        }
+    } else {
+        return {
+            error:true
+        }
+    }
+}
