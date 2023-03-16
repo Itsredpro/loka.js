eb664b94df5ef8962483c0ca9e27dfa1bbe91dab
Itsredpro <thomhmail@gmail.com>
2023-03-16 10:18:56 +0100

    town fulllist support!

--- stable/town.js
+++ stable/town.js
@@ -42,6 +42,47 @@ module.exports.get.byArea = async function(area){
 }
 
 
+module.exports.get.fullList = async function(){
+    var out = {}
+    try {
+        out = await axios.get("https://testapi.lokamc.com/towns?size=1000")
+    } catch(e){
+
+    }
+
+    if (out.data){
+        for (var i = 0; i< out.data.page.totalPages.length; i++){
+            var out = []
+            var error = false
+            try {
+                var res = await axios.get("https://testapi.lokamc.com/towns?size=1000&page=" + i)
+                for (var i2 = 0; i2< res.data._embedded.towns.length; i2++){
+                    out.push(res.data._embedded.towns[i2])
+                }
+            } catch(e){
+                error = true
+            }
+
+            if (error){
+                return {
+                    error:true
+                }
+            } else {
+                return {
+                    error:false,
+                    data:out
+                }
+            }
+        }
+    } else {
+        return {
+            error:true
+        }
+    }
+}
+
+
+
 module.exports.custom = {}
 
 module.exports.custom.userTownSearch = async function(searchQuery){ // Primairly used for user interaction. Town searching, use it like you do with google.
